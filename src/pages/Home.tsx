7266a6bdd1d3f0a5e349b707acacb1fbc97a6952
Mike Hartington <mikehartington@gmail.com>
2023-05-22 11:57:17 -0400

    second demo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,61 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useEffect, useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
-
+import { Directory, Encoding, Filesystem } from '@capacitor/filesystem';
+import {add} from 'ionicons/icons';
 const Home: React.FC = () => {
+  const [notes, setNotes] = useState<any>([])
+  const initFS = async() => {
+    Filesystem.requestPermissions();
+  }
+  const readDir = async() =>{
+    const { files } = await Filesystem.readdir({
+      directory: Directory.Documents,
+      path: 'notes'
+    })
+    setNotes(files);
+  }
+
+  const mkDir = async() =>{
+    await Filesystem.mkdir({
+      directory: Directory.Documents,
+      path: 'notes'
+    }).then(
+      () => console.log('dir made'), 
+      () => console.log('dir already existed'))
+  }
+
+
+  const mkFile = async() => {
+    const data = `${Date.now()}`;
+
+    await Filesystem.writeFile({
+      data,
+      directory: Directory.Documents,
+      path: `notes/${data}.txt`,
+      encoding: Encoding.UTF8
+    }).then(() => readDir())
+  }
+
+  useEffect(() => {
+    const init = async() => {
+      await initFS();
+      await mkDir();
+      await readDir();
+    }
+    init();
+  }, [])
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Blank</IonTitle>
+          <IonButtons slot="end">
+            <IonButton onClick={mkFile}>
+                <IonIcon icon={add}></IonIcon>
+            </IonButton>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,7 +64,11 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        {notes.map((note: File) => (
+          <pre key={note.name}>
+            {JSON.stringify(note, null, 2)}
+          </pre>
+        ))}
       </IonContent>
     </IonPage>
   );
